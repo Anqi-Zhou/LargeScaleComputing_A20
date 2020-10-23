156e848471724088da46297ce5d6bf2271590f20
Anqi Zhou <anqiz1@midway2-login1.rcc.local>
2020-10-23 06:52:14 -0500

    plot_q1

--- Assignment 1/time_plot_q1.py
+++ Assignment 1/time_plot_q1.py
@@ -7,14 +7,14 @@ def plot_time():
   comm = MPI.COMM_WORLD
   rank = comm.Get_rank()
   if rank == 0:
-    time = np.loadtxt("time_question_1.txt")
+    time = np.loadtxt("time_q1.txt")
   
   num_cores = np.arange(20)+1
   plt.plot(num_cores, time)  
   plt.title("Computation Time with Different Number of Cores")
   plt.xlabel("Number of cores") 
   plt.ylabel("Computation time")
-  plt.savefig("time_plot_q1.png")
+  plt.savefig("plot_q1.png")
 
 def main():
   plot_time()
